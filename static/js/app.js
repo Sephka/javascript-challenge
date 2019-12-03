2a3f96ed196fd10d4b91aa3f6dad6e184ecdc9b1
Sephka <32805735+Sephka@users.noreply.github.com>
2019-12-03 00:27:28 -0700

    Uploading correct file

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,42 @@
+// Declare variables
+let tbody = d3.select("tbody");
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+// Creates a function to build the table
+function buildTable(data){
+  // Clear existing data
+  tbody.html("");
+  // Loops through "data"
+  data.forEach((dataRow) => {
+		// Appends 'tr' to 'tbody'
+    let row = tbody.append("tr");
+    // Use 'Object.values' and 'forEach' to iterate throguh values
+    Object.values(dataRow).forEach((val) => {
+      // Appends a cell to the row for each value
+      let cell = row.append("td");
+      cell.text(val);
+    });
+  })
+}
+// An event that triggers a function when the button is clicked
+function handleClick(){
+	// Prevents page refresh
+  d3.event.preventDefault();
+  // Selects HTML input element, gets the value property of that input element
+  let date = d3.select("#datetime").property("value");
+  let filterData = tableData;
+
+  // Checks if a date was entered and filters data using that date
+  if(date) {
+  	// Applies a filter to the table data to only keep rows where datetime value matches the filter value
+    filterData = filterData.filter((row) => row.datetime === date);
+  }
+  // Builds a table with filtered data
+  buildTable(filterData);
+}
+// 'on' function to attach an event to the handler function
+d3.selectAll("#filter-btn").on("click", handleClick);
+// Builds a table with data.js
+buildTable(tableData);
